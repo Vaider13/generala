dd8eb578f1e0c11d6ed2d619cd54bc9af5887550
Vaider13 <pabloquirco@hotmail.com>
2023-02-18 09:49:35 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let dado1 = 0;
+let dado2 = 0;
+let dado3 = 0;
+let dado4 = 0;
+let dado5 = 0;
+let turnoJugador = 0;
+let puntajeJugador1 = 0;
+let puntajeJugador2 = 0;
+
+function lanzarDados() {
+    dado1 = Math.floor(Math.random() * 6) + 1;
+    dado2 = Math.floor(Math.random() * 6) + 1;
+    dado3 = Math.floor(Math.random() * 6) + 1;
+    dado4 = Math.floor(Math.random() * 6) + 1;
+    dado5 = Math.floor(Math.random() * 6) + 1;
+
+};
